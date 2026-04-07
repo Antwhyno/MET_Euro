ed5ad978e2bc2ba42a46209e3c2dab455eddadc9
Antwhyno <lisamonte251@gmail.com>
2026-04-07 18:05:57 +0200

    modif probleme chargement img

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,7 +2,7 @@ from nicegui import ui
 def dessiner_page():
     with ui.row().classes('w-full items-center bg-gray-50'):
         ui.label('Musée du MET').classes('text-5xl font-bold mb-8')
-        ui.image('assets/met.jpg').classes('w-1/2')
+        ui.image('/assets/met_hero.png').classes('w-1/2')
         ui.label('Le MET est un musée situé à New York, fondé en 1870. Il est l\'un des plus grands et des plus importants musées d\'art au monde.').classes('text-xl mb-8')
 
     with ui.card().classes('w-1/2 bg-red-100 h-64'):
--- pages/page_2.py
+++ pages/page_2.py
@@ -7,4 +7,4 @@ app.add_static_files('/assets', 'assets')
 def dessiner_page():
     with ui.row().classes('w-full items-center bg-gray-50'):
         ui.card().classes('w-1/2 bg-red-100 h-64')
-        ui.image('/assets/met_hero.jpg').classes('w-1/2 rounded-lg shadow-lg')
+        ui.image('/assets/met_hero.png').classes('w-1/2 rounded-lg shadow-lg')

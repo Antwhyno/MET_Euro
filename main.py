f24f76f6c80f0236766dc26cb96463e188b899b8
Antwhyno <lisamonte251@gmail.com>
2026-04-07 17:42:28 +0200

    debut concret

---
+++ main.py
@@ -0,0 +1,19 @@
+# chef d'orchestre
+from nicegui import ui
+
+from pages import page_1
+# 1. On crée le menu qui restera accroché en haut de l'écran pendant qu'on descend
+with ui.header().classes('bg-slate-900 justify-between items-center p-4'):
+    ui.label('Mon Projet Artistique').classes('text-xl font-bold')
+    with ui.row():
+        ui.link('Accueil', '/').classes('hover:text-blue-400')
+        ui.link('Le Projet', '/projet').classes('hover:text-blue-400')
+        ui.link('Conclusion', '/conclusion').classes('hover:text-blue-400')
+
+# 3. La page d'accueil (qui va tout empiler)
+@ui.page('/')
+def page_principale():
+    page_1.dessiner_page()
+
+# 4. On lance le moteur (avec les réglages Hugging Face prêts au cas où)
+ui.run(title="Mon Portfolio", favicon="🚀", host="0.0.0.0", port=7860)
---
+++ pages/page_1.py
@@ -0,0 +1,10 @@
+from nicegui import ui
+def dessiner_page():
+    with ui.row().classes('w-full items-center bg-gray-50'):
+        ui.label('Musée du MET').classes('text-5xl font-bold mb-8')
+        ui.image('assets/met.jpg').classes('w-1/2')
+        ui.label('Le MET est un musée situé à New York, fondé en 1870. Il est l\'un des plus grands et des plus importants musées d\'art au monde.').classes('text-xl mb-8')
+
+    with ui.card().classes('w-1/2 bg-red-100 h-64'):
+        ui.label('Je suis la grande carte de gauche !')
+        
